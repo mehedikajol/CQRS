0b3da2f0a7e7caf49490cb35fe506cebf3984713
Mehedi Hasan Kajol <mehedi.kajol.ice@gmail.com>
2023-04-03 13:00:41 +0600

    core implemented

---
+++ CQRS.Core/Repositories/Query/Base/IQueryRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace CQRS.Core.Repositories.Query.Base
+{
+    public interface IQueryRepository<T> where T : class
+    {
+        // generic methods for all queries
+    }
+}
---
+++ CQRS.Core/Repositories/Query/ICustomerQueryRepository.cs
@@ -0,0 +1,12 @@
+﻿using CQRS.Core.Entities;
+using CQRS.Core.Repositories.Query.Base;
+
+namespace CQRS.Core.Repositories.Query
+{
+    public interface ICustomerQueryRepository : IQueryRepository<Customer>
+    {
+        Task<IReadOnlyList<Customer>> GetAllCustomersAsync();
+        Task<Customer> GetCustomerByIdAsync(Guid id);
+        Task<Customer> GetCustomerByEmailAsync(string email);
+    }
+}

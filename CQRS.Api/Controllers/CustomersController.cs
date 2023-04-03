127a3ab10ae110b9aeedf606e994bf73cde28039
Mehedi Hasan Kajol <mehedi.kajol.ice@gmail.com>
2023-04-03 15:29:33 +0600

    service error

---
+++ CQRS.Api/Controllers/CustomersController.cs
@@ -0,0 +1,86 @@
+﻿using CQRS.Application.Commands;
+using CQRS.Application.Queries;
+using CQRS.Application.Responses;
+using CQRS.Core.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CQRS.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CustomersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<List<Customer>> Get()
+        {
+            return await _mediator.Send(new GetAllCustomerQuery());
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<Customer> GetById(Guid id)
+        {
+            return await _mediator.Send(new GetCustomerByIdQuery(id));
+        }
+
+        [HttpGet("{email}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<Customer> GetByEmail(string email)
+        {
+            return await _mediator.Send(new GetCustomerByEmailQuery(email));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<CustomerResponse>> CreateCustomer([FromBody] CreateCustomerCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+
+        [HttpPut("EditCustomer/{id}")]
+        public async Task<IActionResult> EditCustomer(Guid id, [FromBody] EditCustomerCommand command)
+        {
+            try
+            {
+                if (command.Id == id)
+                {
+                    var result = await _mediator.Send(command);
+                    return Ok(result);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("DeleteCustomer/{id}")]
+        public async Task<ActionResult> DeleteCustomer(Guid id)
+        {
+            try
+            {
+                string result = string.Empty;
+                result = await _mediator.Send(new DeleteCustomerCommand(id));
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
